ba62cb3d9af4c867d39fe88b8a59a4cb932646d7
FineArchs <133759614+FineArchs@users.noreply.github.com>
2023-10-18 20:15:34 +0900

    Update api.ts

--- packages/frontend/src/scripts/api.ts
+++ packages/frontend/src/scripts/api.ts
@@ -53,7 +53,7 @@ export function api<E extends keyof Misskey.Endpoints, P extends Misskey.Endpoin
 
 export async function apiExternal<E extends keyof Misskey.Endpoints, P extends Misskey.Endpoints[E]['req']>(host: string, endpoint: E, data: P = {} as any, token?: string | null | undefined, signal?: AbortSignal): Promise<Misskey.Endpoints[E]['res']> {
 	if (endpoint.includes('://')) throw new Error('invalid endpoint');
-	const [ hostName: string, hostUrl: string ] = await (new Promise(resolve => resolve(URL(host))))
+	const [ hostName, hostUrl ] = await (new Promise(resolve => resolve(URL(host))))
 		.then(url => {
 			if (['http:', 'https:'].includes(url.protocol)) return [ url.host, host ];
 			else throw new Error('Only http and https are allowed as url protocol.');
