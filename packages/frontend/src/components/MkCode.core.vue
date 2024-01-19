f5fbe993636e88c271f83efc77186733ca919887
FineArchs <133759614+FineArchs@users.noreply.github.com>
2024-01-19 16:31:58 +0900

    debug

--- packages/frontend/src/components/MkCode.core.vue
+++ packages/frontend/src/components/MkCode.core.vue
@@ -25,6 +25,7 @@ const html = ref<string | null>(null);
 
 // setupの中でawaitするとロードが遅延されそう（多分）なのでasync関数内で実行
 if (props.lang !== undefined) (async () => {
+	throw 'hoge';
 	const highlighter = await getHighlighter();
 	const codeLang = await fetchLanguage(highlighter, props.lang);
 	if (codeLang === null) return;
