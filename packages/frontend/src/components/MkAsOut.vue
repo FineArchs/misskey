c5ae5724adac35af69278a39c054bd04f5ef8938
FineArchs <133759614+FineArchs@users.noreply.github.com>
2024-03-14 03:59:42 +0900

    Create MkAsOut.vue

---
+++ packages/frontend/src/components/MkAsOut.vue
@@ -0,0 +1,66 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+<template>
+<div :class="$style.root">
+	<div v-for="log in model.logs" :key="log.id" :class="$style[log.type]">{{ log.text }}</div>
+</div>
+</template>
+
+<script lang="ts">
+export AsOutTL;
+</script>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { values, utils, errors } from '@syuilo/aiscript';
+
+const model = defineModel<AsOutTL>({ required: true });
+
+type AsOutLog = {
+  type: 'out' | 'log' | 'err',
+  text: string,
+  id: string,
+};
+
+class AsOutTL {
+  constructor(){}
+  readonly logs = ref<AsOutLog[]>([]);
+  private push(type: AsOutLog['type'], text: string) {
+    logs.value.push({type, text, id: Math.random().toString()});
+  }
+  out(value: values.Value): void {
+    push('out', value.type === 'str' ? value.value : utils.valToString(value));
+  }
+  log(type: string, params: values.Value): void {
+    switch (type) {
+      case 'end':
+        push('log', utils.valToString(params.val, true));
+      default: break;
+    }
+  }
+  logManually(mes: string): void {
+    push('log', mes);
+  }
+  err(err: errors.AiScriptError): void {
+    push('err', `${err}`);
+  }
+}
+</script>
+
+<style lang="scss" module>
+.root {
+	padding: 16px;
+}
+
+.out {
+}
+.log {
+  opacity: 0.7;
+}
+.err {
+  color: var(--error);
+}
+</style>
