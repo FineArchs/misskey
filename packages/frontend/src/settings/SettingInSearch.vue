c9ae63a153beb73c7a246f0fd00e0f938a17dc72
FineArchs <133759614+FineArchs@users.noreply.github.com>
2024-08-12 06:15:15 +0900

    Create SettingInSearch.vue

---
+++ packages/frontend/src/settings/SettingInSearch.vue
@@ -0,0 +1,20 @@
+<!--
+SPDX-FileCopyrightText: syuilo and misskey-project
+SPDX-License-Identifier: AGPL-3.0-only
+-->
+
+<template>
+<MkSwitch v-if="def.kind == 'switch' v-model="store">{{ def.title }}</MkSwitch>
+</template>
+
+<script lang="ts" setup>
+import { settings } from '@/settings/settings.ts';
+import { defaultStore } from '@/store.js';
+
+const props = defineProps<{
+	id: keyof typeof settings;
+}>();
+
+const def = settings[props.id];
+const store = computed(defaultStore.makeGetterSetter(props.id));
+</script>
