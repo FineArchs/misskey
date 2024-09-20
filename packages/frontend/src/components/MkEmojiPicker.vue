a218e1fb6e9fbd57aa2d797eceb28924aee43c43
FineArchs <cicada1003@gmail.com>
2024-09-20 17:01:07 +0900

    tabs

--- packages/frontend/src/components/MkEmojiPicker.vue
+++ packages/frontend/src/components/MkEmojiPicker.vue
@@ -21,11 +21,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 	<!-- FirefoxのTabフォーカスが想定外の挙動となるためtabindex="-1"を追加 https://github.com/misskey-dev/misskey/issues/10744 -->
 	<div ref="emojisEl" class="emojis" tabindex="-1">
 		<section class="result">
-			<div :class="$styles.tabs">
-				<div @click="emojiTab = 'custom'">{{ i18n.ts.emojitabCustom }}</div>
-				<div @click="emojiTab = 'unicode'">{{ i18n.ts.emojitabUnicode }}</div>
-			</div>
-			<div v-if="emojiTab = 'custom' && searchResultCustom.length > 0" class="body">
+			<div v-if="tab === 'custom' && searchResultCustom.length > 0" class="body">
 				<button
 					v-for="emoji in searchResultCustom"
 					:key="emoji.name"
@@ -38,7 +34,7 @@ SPDX-License-Identifier: AGPL-3.0-only
 					<MkCustomEmoji class="emoji" :name="emoji.name" :fallbackToImage="true"/>
 				</button>
 			</div>
-			<div v-if="emojiTab = 'unicode' && searchResultUnicode.length > 0" class="body">
+			<div v-if="tab === 'unicode' && searchResultUnicode.length > 0" class="body">
 				<button
 					v-for="emoji in searchResultUnicode"
 					:key="emoji.name"
@@ -183,7 +179,6 @@ const q = ref<string>('');
 const searchResultCustom = ref<Misskey.entities.EmojiSimple[]>([]);
 const searchResultUnicode = ref<UnicodeEmojiDef[]>([]);
 const tab = ref<'index' | 'custom' | 'unicode' | 'tags'>('index');
-const emojiTab = ref<'custom' | 'unicode'>('custom')
 
 const customEmojiFolderRoot: CustomEmojiFolderTree = { value: '', category: '', children: [] };
 
@@ -658,7 +653,7 @@ defineExpose({
 
 	> .tabs {
 		display: flex;
-		display: none;
+		// display: none;
 
 		> .tab {
 			flex: 1;
